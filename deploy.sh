afc36adde9266dfae8999e73edd10ae76c068d3a
Eric Berry <coderberry@gmail.com>
2019-04-07 12:49:12 -0600

    Add deploy script, remove netlify

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-echo 'read.cesletter.org' > CNAME
+# echo 'read.cesletter.org' > CNAME
 
 git init
 git add -A
